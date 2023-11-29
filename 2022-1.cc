a53b7aeff5d192e108b5333f060dc6bc7d995142
Helen Chang <helenchang@Helens-MacBook-Pro.local>
2023-11-29 18:06:58 -0500

    Add solution for 2022 day 1 part 2

--- 2022-1.cc
+++ 2022-1.cc
@@ -2,7 +2,10 @@
 
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <string>
+#include <string_view>
+#include <vector>
 
 using std::cerr;
 using std::string;
@@ -23,6 +26,28 @@ int get_max_set(std::istream& input) {
   return max;
 }
 
+const int MAX_SUM = 3;
+int get_top_3_sum(std::istream& input) {
+  std::priority_queue<int> max_pq;
+  string tmp = "";
+  int tmp_sum = 0;
+  while (!input.eof()) {
+    std::getline(input, tmp);
+    if (tmp != "") {
+      tmp_sum += atoi(tmp.c_str());
+    } else {
+      max_pq.push(tmp_sum);
+      tmp_sum = 0;
+    }
+  }
+  int max_sum = 0;
+  for (int i = 0; i < MAX_SUM; i++) {
+    max_sum += max_pq.top();
+    max_pq.pop();
+  }
+  return max_sum;
+}
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     std::cerr << "This program expects one parameter that is the input file to "
@@ -34,6 +59,8 @@ int main(int argc, char* argv[]) {
     std::cerr << "Failed to open file.\n";
     return 1;
   }
-  std::cout << "max is " << get_max_set(input);
+  // input is passed by reference so can't be shared between two methods
+  // std::cout << "max is " << get_max_set(input);
+  std::cout << "top 3 sum is " << get_top_3_sum(input);
   return 0;
 }
