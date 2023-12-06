0cf4b423767edc6d0b641efd95810be3a8d74757
Helen Chang <helenchang@Helens-MacBook-Pro.local>
2023-12-06 17:50:03 -0500

    Add solution to 2023 day 5 part 2

--- 2023day5.cc
+++ 2023day5.cc
@@ -26,16 +26,18 @@ const int LIGHT_TO_TEMP = 4;
 const int TEMP_TO_HUMIDITY = 5;
 const int HUMIDITY_TO_LOC = 6;
 
-vector<map<long, long>> maps = {{}, {}, {}, {}, {}, {}, {}};
-std::vector<long> seeds = {};
-
 struct Range {
   long start;
   long end;
   long add;
 };
 
+std::vector<long> seeds = {};
+std::vector<Range> seeds_as_range = {};
+
 vector<vector<Range>> ranges = {{{}}, {{}}, {{}}, {{}}, {{}}, {{}}, {{}}};
+vector<vector<Range>> reverse_ranges = {{{}}, {{}}, {{}}, {{}},
+                                        {{}}, {{}}, {{}}};
 
 std::vector<long> string_to_int(const vector<string> in) {
   std::vector<long> out = {};
@@ -54,6 +56,39 @@ long get_corresponding(vector<Range> ranges, long key) {
   return key;
 }
 
+bool seed_in_range(const vector<Range>& ranges, long seed) {
+  for (auto range : ranges) {
+    if (seed >= range.start && seed < range.end) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// this is not optimized and takes some time.
+long part2() {
+  long location = 0;
+  long stop_location = 324724204; // answer to pt 1
+  while (location != stop_location ) {
+    long humidity = get_corresponding(reverse_ranges[HUMIDITY_TO_LOC], location);
+    long temp = get_corresponding(    reverse_ranges[TEMP_TO_HUMIDITY], humidity);
+    long light = get_corresponding(   reverse_ranges[LIGHT_TO_TEMP], temp);
+    long water = get_corresponding(   reverse_ranges[WATER_TO_LIGHT], light);
+    long fert = get_corresponding(    reverse_ranges[FERT_TO_WATER], water);
+    long soil = get_corresponding(    reverse_ranges[SOIL_TO_FERT], fert);
+    long seed = get_corresponding(    reverse_ranges[SEED_TO_SOIL], soil);
+    if (seed_in_range(seeds_as_range, seed)) {
+      printf(
+          "seed %ld, soil %ld, fert %ld, water %ld, light %ld, temp %ld, "
+          "humidity %ld,"
+          "location %ld\n",
+          seed, soil, fert, water, light, temp, humidity, location);
+      return location;
+    }
+    location++;
+  }
+}
+
 long part1() {
   int min_location = INT_MAX;
   for (long seed : seeds) {
@@ -64,11 +99,11 @@ long part1() {
     long temp = get_corresponding(ranges[LIGHT_TO_TEMP], light);
     long humidity = get_corresponding(ranges[TEMP_TO_HUMIDITY], temp);
     long location = get_corresponding(ranges[HUMIDITY_TO_LOC], humidity);
-    printf(
-        "seed %ld, soil %ld, fert %ld, water %ld, light %ld, temp %ld, "
-        "humidity %ld,"
-        "location %ld\n",
-        seed, soil, fert, water, light, temp, humidity, location);
+    // printf(
+    //     "seed %ld, soil %ld, fert %ld, water %ld, light %ld, temp %ld, "
+    //     "humidity %ld,"
+    //     "location %ld\n",
+    //     seed, soil, fert, water, light, temp, humidity, location);
     min_location = location < min_location ? location : min_location;
   }
   return min_location;
@@ -92,11 +127,13 @@ int main(int argc, char* argv[]) {
   std::vector<string> seeds_as_string(
       absl::StrSplit(tmp, " ", absl::SkipEmpty()));
   seeds = string_to_int(seeds_as_string);
+  for (int i = 0; i < seeds.size(); i = i + 2) {
+    seeds_as_range.emplace_back(Range{seeds[i], seeds[i] + seeds[i + 1], 0});
+  }
 
   int map_idx = -1;
   while (!input_stream.eof()) {
     std::getline(input_stream, tmp);
-    // printf("%s\n", tmp.c_str());
     if (tmp.empty()) continue;
     if (absl::StrContains(tmp, ":")) {
       map_idx++;
@@ -107,10 +144,13 @@ int main(int argc, char* argv[]) {
     long dest_start = input_vec[DEST_INDEX];
 
     ranges[map_idx].emplace_back(Range{source_start,
-                                  input_vec[RANGE_INDEX] + source_start,
-                                  dest_start - source_start});
-
+                                       input_vec[RANGE_INDEX] + source_start,
+                                       dest_start - source_start});
+    reverse_ranges[map_idx].emplace_back(
+        Range{dest_start, input_vec[RANGE_INDEX] + dest_start,
+              source_start - dest_start});
   }
   printf("answer to part 1: %ld\n", part1());
+  printf("answer to part 2: %ld\n", part2());
   return 0;
 }
